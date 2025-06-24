202d50a6da491beb9ef710db0bd4dbc1fa32487a
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-06-25 00:07:23 +0600

    basic redux intro

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { decrement, increment } from './redux/features/counter/counterSlice';
+import { useAppDispatch, useAppSelector } from './redux/hook';
+
+function App() {
+  const dispatch = useAppDispatch();
+  const {count} = useAppSelector((state) => state.counter);
+
+  const handleIncrement = (amount: number) => {
+    dispatch(increment(amount));
+  }
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  }
+
+  return (
+    <div>
+      <h1>Counter with Redux</h1>
+      <button onClick={() => handleIncrement(1)}>Increment</button>
+      <button onClick={() => handleIncrement(5)}>Increment by 5</button>
+      <div>{count}</div>
+      <button onClick={handleDecrement}>Decrement</button>
+    </div>
+  )
+}
+
+export default App;
